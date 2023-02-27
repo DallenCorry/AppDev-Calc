f074830d4bef5a20b67b5404b222de43f1283341
Dallen Corry <dallenrobert@gmail.com>
2023-02-27 08:54:41 -0700

    Created all buttons and text views

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val clear: Button = findViewById(R.id.btnClear)
+        val txtField:TextView = findViewById(R.id.textView1)
+
+
+
+        clear.setOnClickListener {
+            txtField.text = ""
+        }
     }
 }
